3f25e5cdb7a15891b8a3bcf7afb690fcbf5b8c3a
liyeon <fldus612@gmail.com>
2023-07-25 17:22:00 +0900

    🍂 sprj02 └pom.xml에 xml 제외 plugin 추가, CGLIB 추가 @Config~사용하려면 추가해야함 └AppConfig : xml의 bean 객체를 대신하는 어노테이션추가 후  Bean 어노테이션으로 객체 등록하기
    --  Day 67 Spring

---
+++ sprj02/src/main/java/com/tech/ex/AppConfig.java
@@ -0,0 +1,26 @@
+package com.tech.ex;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// xml config를 대신하는 어노테이션
+// config 어노테이션을 사용하려면 CGLIB 가 필요하다.
+@Configuration
+public class AppConfig {
+//	빈등록
+	@Bean
+	public Student student1() {
+		Student student = new Student("홍길동", "20");
+		student.setGradeNum("5학년");
+		student.setClassNum("computer");
+		return student;
+	}
+	
+	@Bean
+	public Student student2() {
+		Student student = new Student("홍길동2", "21");
+		student.setGradeNum("6학년");
+		student.setClassNum("computerAA");
+		return student;
+	}
+}
---
+++ sprj02/src/main/java/com/tech/ex/MainClass.java
@@ -0,0 +1,19 @@
+package com.tech.ex;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainClass {
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext ctx= new AnnotationConfigApplicationContext(AppConfig.class);
+		Student student1=ctx.getBean("student1",Student.class);
+		System.out.println(student1.getName());
+		System.out.println(student1.getAge());
+		System.out.println(student1.getGradeNum());
+		System.out.println(student1.getClassNum());
+		Student student2=ctx.getBean("student2",Student.class);
+		System.out.println(student2.getName());
+		System.out.println(student2.getAge());
+		System.out.println(student2.getGradeNum());
+		System.out.println(student2.getClassNum());
+	}
+}
